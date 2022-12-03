47490e0762b5e08af730037f35557da9036ef57a
Emily <emelizadaniels@gmail.com>
2022-12-03 15:25:19 -0500

    commit tree 306eae78909c0e14484f373c2efc1890b212e696

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,31 @@
-# your code goes here
+require 'pry'
+class Person
+    attr_reader :name
+    attr_accessor :bank_account, :happiness, :hygiene
+
+def initialize
+    @name = name
+    @bank_account = 25
+    @happiness = 8
+    @hygiene = 8
+end
+# using = makes this a setting method
+
+def happiness=(value)
+    @happiness = if value > 10
+        10
+    elsif value < 0
+        0
+    else
+        value
+    end
+def hygiene=(value)
+    @value =  if value > 10
+        10 
+    elsif value < 0
+        0
+    else
+        value
+end
+end
+binding.pry
